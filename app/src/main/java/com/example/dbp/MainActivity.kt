05097d28a28a6eb2422fe744ac6fe1f7d334fdcf
alef7893 <82951714+alef7893@users.noreply.github.com>
2023-07-04 01:03:51 -0500

    Sube

--- app/src/main/java/com/example/dbp/MainActivity.kt
+++ app/src/main/java/com/example/dbp/MainActivity.kt
@@ -2,51 +2,105 @@ package com.example.dbp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import android.widget.Button
 import android.widget.PopupMenu
-import android.view.MenuInflater
-import android.view.SubMenu
+
+import android.view.Menu
+import android.widget.ListView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
-    var isBuscarLibrosExpanded: Boolean = false
+    private val dataList = mutableListOf<DataItem>()
+    private lateinit var requestQueue: RequestQueue
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val updateMsgButton = findViewById<Button>(R.id.update_msg_button)
-        val startNewActivityButton = findViewById<Button>(R.id.button2)
         val textView =  findViewById<TextView>(R.id.textView)
-
-        updateMsgButton.setOnClickListener {
-            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-            val current = LocalDateTime.now().format(formatter)
-            textView.text = current
-        }
-
-        startNewActivityButton.setOnClickListener {
-            val intent = Intent(this, Category::class.java)
-            startActivity(intent)
-        }
-
         val btnOpciones = findViewById<Button>(R.id.btnOpciones)
-        val opcionesMenu = PopupMenu(this, btnOpciones)
-        val inflater: MenuInflater = opcionesMenu.menuInflater
-        inflater.inflate(R.menu.menu_opciones, opcionesMenu.menu)
-        val buscarLibrosSubMenu: SubMenu? = opcionesMenu.menu.findItem(R.id.menu_buscar_libros).subMenu
 
-        opcionesMenu.setOnMenuItemClickListener { item ->
-            val opcionSeleccionada: String = item.title.toString()
-            textView.text = opcionSeleccionada
-            if (opcionSeleccionada == "Contactos") {
-                textView.text = "Contactos"
+        btnOpciones.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view)
+            popupMenu.inflate(R.menu.menu_opciones)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_buscar_libros -> {
+                        if (popupMenu.menu.findItem(R.id.menu_buscar_todos) == null) {
+                            popupMenu.menu.add(Menu.NONE, R.id.menu_buscar_todos, Menu.NONE, "Buscar todos")
+                            popupMenu.menu.add(Menu.NONE, R.id.menu_matematicas, Menu.NONE, "Matemáticas")
+                            popupMenu.menu.add(Menu.NONE, R.id.menu_programacion, Menu.NONE, "Programación")
+                            popupMenu.menu.add(Menu.NONE, R.id.menu_comunicacion, Menu.NONE, "Comunicación")
+                        } else {
+                            popupMenu.menu.removeItem(R.id.menu_buscar_todos)
+                            popupMenu.menu.removeItem(R.id.menu_matematicas)
+                            popupMenu.menu.removeItem(R.id.menu_programacion)
+                            popupMenu.menu.removeItem(R.id.menu_comunicacion)
+                        }
+                    }
+                    R.id.menu_buscar_todos  -> { getBooks() }
+                    R.id.menu_matematicas -> { getBooks(1) }
+                    R.id.menu_programacion  -> { getBooks(2) }
+                    R.id.menu_comunicacion -> { getBooks(3) }
+                    R.id.menu_contactos -> { textView.text = item.title.toString() }
+                }
+                true
             }
-            true
+            popupMenu.show()
         }
-        btnOpciones.setOnClickListener { opcionesMenu.show() }
+        getBooks()
+    }
+    private fun getBooks() {
+        consultRoute("http://192.168.0.20:5000/books")
+    }
+
+    private fun getBooks(idCategory: Int) {
+        consultRoute("http://192.168.0.20:5000/books/categorias/" + idCategory.toString())
+    }
+
+    private fun consultRoute(url: String) {
+        val listView = findViewById<ListView>(R.id.postListView)
+        val adapter = DataAdapter(dataList)
+        listView.adapter = adapter
+        requestQueue = Volley.newRequestQueue(this)
+        val textView =  findViewById<TextView>(R.id.textView)
+        val jsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val parsedData = parseResponse(response)
+                textView.text = url
+                dataList.clear()
+                dataList.addAll(parsedData)
+                adapter.notifyDataSetChanged()
+            },
+            { error -> println(error.toString()) })
+        requestQueue.add(jsonArrayRequest)
+    }
+
+    private fun parseResponse(response: JSONArray): List<DataItem> {
+        val dataList = mutableListOf<DataItem>()
+        try {
+            for (i in 0 until response.length()) {
+                val jsonObject = response.getJSONObject(i)
+                val autor = jsonObject.getString("autor")
+                val descripcion = jsonObject.getString("descripcion")
+                val id = jsonObject.getInt("id")
+                val idCategory = jsonObject.getInt("id_category")
+                val idUsuario = jsonObject.getInt("id_usuario")
+                val precio = jsonObject.getDouble("precio")
+                val titulo = jsonObject.getString("titulo")
+                val dataItem = DataItem(autor, descripcion, id, idCategory, idUsuario, precio, titulo)
+                dataList.add(dataItem)
+            }
+        } catch (e: Exception) {e.printStackTrace() }
+        return dataList
     }
 }
